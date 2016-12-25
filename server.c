0740b03f3fc230cecadd29c207f2224e299db604
gravedigger95 <masalusicstefan@gmail.com>
2016-12-26 00:04:31 +0100

    Update server.c

--- server.c
+++ server.c
@@ -8,7 +8,7 @@
  Skolska godina: 2016/2017
  Semestar:       Zimski (V)
  
- Ime fajla:      server.c
+ website fajla:      server.c
  Opis:           TCP/IP server
  
  Platforma:      Raspberry Pi 2 - Model B
@@ -26,11 +26,14 @@
 #define DEFAULT_PORT   27015
 #define BUF 128
 #define TOT 20
-#define filename "/home/pi/Documents/ra193/ORMDNS/lab_4/src/lista.txt"
-#define filename1 "/home/pi/Documents/ra193/ORMDNS/lab_4/src/rootlista.txt"
-struct klijenti {
+#define filename "/home/pi/Projekat/ORMDNS/lab_4/src/lista.txt"
+#define filename1 "/home/pi/Projekat/ORMDNS/lab_4/src/rootlista.txt"
+
+void DoProcessing(int client_sock, int root_sock);
+
+struct clients {
 	char ip[100];
-	char ime[100];
+	char website[100];
 };
 
 int main(int argc, char *argv[]) {
@@ -38,7 +41,6 @@ int main(int argc, char *argv[]) {
 	int socket_desc, client_sock, c, root_sock;
 	struct sockaddr_in server, client, root_server;
 	int pid;
-	int n = 0;
 
 	//Create socket
 
@@ -63,8 +65,7 @@ int main(int argc, char *argv[]) {
 	root_server.sin_family = AF_INET;
 	root_server.sin_port = htons(27014);
 
-	if (connect(root_sock, (struct sockaddr *) &root_server,
-			sizeof(root_server)) < 0) {
+	if (connect(root_sock, (struct sockaddr *) &root_server, sizeof(root_server)) < 0) {
 		perror("connect failed. Error");
 		return 1;
 	}
@@ -89,8 +90,7 @@ int main(int argc, char *argv[]) {
 	//accept connection from an incoming client
 	while (1) {
 
-		client_sock = accept(socket_desc, (struct sockaddr *) &client,
-				(socklen_t*) &c);
+		client_sock = accept(socket_desc, (struct sockaddr *) &client,(socklen_t*) &c);
 		if (client_sock < 0) {
 			perror("accept failed");
 			return 1;
@@ -106,7 +106,7 @@ int main(int argc, char *argv[]) {
 		if (pid == 0) {
 
 			close(socket_desc);
-			doprocessing(client_sock, root_sock);
+			DoProcessing(client_sock, root_sock);
 
 			return (0);
 		} else {
@@ -121,230 +121,143 @@ int main(int argc, char *argv[]) {
 
 }
 
-void doprocessing(int client_sock, int root_sock) {
+void DoProcessing(int client_sock, int root_sock) {
+
 	int read_size;
 	char client_message[DEFAULT_BUFLEN];
-	//Receive a message from client
 	int k;
 	char buff[100];
-	char line[100];
-	int x, y;
-	int flag = 0;
+	int x, y; //iterators for cleaning arrays
+	int inLocalList = 0; //flag for checking is ip or website sent from root server already in local server list
 	char* ptr;
-	char* poruka = "Ne mogu da razresim upit.\n Saljem korenskom...\n";
-	int n = 0;
+	int n = 0; //flag for checking is ip or website in local server list
 	int num;
-	int total;
-	int xyz;
+	int total; //total number of elements in arrays
+	int invalid; //flag for checking if client typed invalid ip or website
 	int i = 0;
-	char ip_s[TOT][BUF];
-	char names[TOT][BUF];
-	int nje;
-	int flag_ip, flag_name;
-		char* poruka1="Ne postoji sajt ili IP u listi";
-	struct klijenti klijent;
-		
+	char ips[TOT][BUF];
+	char websites[TOT][BUF];
+	int flag_ip, flag_website; //flags for knowing what to return to client(ip or website)
+	char* message = "Website or IP doesn't exist";
+	struct clients client;
+
+	//Receive a message from client
+
 	while ((read_size = recv(client_sock, client_message, DEFAULT_BUFLEN, 0))
-			> 0) {	
-i=0;
+			> 0) {
+		i = 0;
 		FILE *fp = fopen(filename, "r");
-		printf("STAMPAM LISTU\n\n");
-		while (fscanf(fp, "%s %s\n", klijent.ime, klijent.ip) != EOF) {
-			ip_s[i][strlen(klijent.ip)] = '\0';
-			strcpy(ip_s[i], klijent.ip);
-			names[i][strlen(klijent.ime)] = '\0';
-			strcpy(names[i], klijent.ime);
-			printf("%s\n",names[i]);
-			printf("%s\n",ip_s[i]);
+		//scan file and split each line to website and ip and fill arrays
+		while (fscanf(fp, "%s %s\n", client.website, client.ip) != EOF) {
+			ips[i][strlen(client.ip)] = '\0';
+			strcpy(ips[i], client.ip);
+			websites[i][strlen(client.website)] = '\0';
+			strcpy(websites[i], client.website);
 			i++;
 		}
-	
-printf("\nSkenirao fajl i upisao u niz\n");
-	total = i;
+
+		total = i;
 		fclose(fp);
-	flag_ip=0;
-	flag_name=0;
-		printf("Bytes received: %d\n", read_size);
+		flag_ip = 0;
+		flag_website = 0;
 		*(client_message + read_size) = '\0';
-		printf("Messagee: %s\n", client_message);
 		n = 0;
+		//iterate through arrays and compare with ip's and websites arrays
 		for (k = 0; k < total; k++) {
-			if (strcmp(client_message, names[k]) == 0) {
+			//client typed website, expecting ip
+			if (strcmp(client_message, websites[k]) == 0) {
 				
-				printf("Sajt koji si uneo se nalazi u listi(strcmp):\n");
-				printf("%s\n",client_message);
-				printf("%s\n",names[k]);
-				printf("IP tog sajta je:\n");				
-				printf("--------------\n");
-				printf("%s", ip_s[k]);
-				printf("\t%d\n",strlen(ip_s[k]));
-				printf("------------\n");
-				
-				if ((n = send(client_sock, ip_s[k], strlen(ip_s[k]), 0))
-						< 0) {
+				//send ip to client
+				if ((n = send(client_sock, ips[k], strlen(ips[k]), 0)) < 0) {
 					puts("Send failed\n");
 					return 1;
-				} 
-
-				printf("Vratio sam ip unetog sajta klijentu\n");
-
-				printf("%d\n", n);
+				}
 				break;
 
 			}
-			
 
-			 if(strcmp(client_message,ip_s[k])==0){
-				printf("IP koji si uneo se nalazi u listi(strcmp):\n");
-				printf("Sajt je:\n");				
-				printf("%s\n",names[k]);
-			 if((n=send(client_sock,names[k],strlen(names[k]),0))<0){
-			 puts("Send failed\n");
-			 return 1;
-			 }
-			printf("Vratio sam naziv sajta klijentu\n");
-			 printf("%d\n",n);
-			 
-			 }
-			 
+			//client typed ip, expecting website
+			if (strcmp(client_message, ips[k]) == 0) {
+				//send website to client
+				if ((n = send(client_sock, websites[k], strlen(websites[k]), 0))
+						< 0) {
+					puts("Send failed\n");
+					return 1;
+				}
+			}
 		}
-
+		//site or ip not in list
 		if (n == 0) {
-			printf("Sajt ili IP se ne nalazi u lokalnoj listi!!!\n");
-			if (send(root_sock, client_message, strlen(client_message), 0)
-					< 0) {
+			//proceed request to root server
+			if (send(root_sock, client_message, strlen(client_message), 0) < 0) {
 				puts("Send failed");
 				return 1;
 			}
-			printf("Saljem korenskom\n");
-			
-			
+			//printf("Saljem korenskom\n");
+
+			//receive response from root server
 			if ((num = recv(root_sock, buff, sizeof(buff), 0)) != -1) {
-				buff[num]='\0';
-				puts(buff);
-				puts(poruka1);
-				if((xyz=strcmp(buff,poruka1))!=0){
-				printf("%d\n",xyz);
 				buff[num] = '\0';
-				printf("Korenski mi vraca celu liniju sa sajtom i IPem\n");
-				printf("Message received: %s\nNumber of bytes received: %d\n",
-						buff, num);
 				
+				//if client typed correct input
+				if ((invalid = strcmp(buff, message)) != 0) {
+					//printf("%d\n",invalid);
+					buff[num] = '\0';
+		
+					//if line not in local list expand list with new website
+					if (inLocalList == 0) {
+						flag_website = 0;
+						flag_ip = 0;
+						
+						FILE *fp2 = fopen(filename, "a+");
+						fprintf(fp2, "%s\n", buff);
+						fclose(fp2);
+			
+						//split string received from root server to website and ip
+						strtok_r(buff, " ", &ptr);
+						
+						//check if client typed website
+						if (strcmp(client_message, buff) == 0) {
+							flag_website = 1;
+						}
+						//check if client typed ip
+						if (strcmp(client_message, ptr) == 0) {
+							flag_ip = 1;
+						}
+				
+						//return ip to client
+						if (flag_website == 1) {
+							if ((send(client_sock, ptr, strlen(ptr), 0)) < 0) {
+								puts("Send failed\n");
+								return 1;
+							}
+						} else if (flag_ip == 1) {
+							//return website to client
+							if ((send(client_sock, buff, strlen(buff), 0)) < 0) {
 
-
-
-
-							FILE *fp4 = fopen(filename, "r");
-					printf("\nSTAMPAM LISTU PRE UPISA\n\n");
-				while (fgets(line, sizeof(line), fp4) != NULL) {
-					flag = 0;
-					printf("%s\n",line);					
-					if (strcmp(buff, line) == 0) {
-						flag++;
-						break;
+								puts("Send failed\n");
+								return 1;
+							}
+						}
 					}
 
-				}
-				fclose(fp4);
-				printf("Iscitao sam fajl i proverio da li je sajt vec u listi\n");
+					bzero(buff, sizeof(buff));
+				} else {
+					if ((send(client_sock, "Invalid!!!\n",strlen("Invalid!!!\n"), 0)) < 0) {
 
-				if (flag == 0) {
-					flag_name=0;
-					flag_ip=0;			
-					printf("Sajt nije u listi\n");
-									FILE *fp2 = fopen(filename, "a+");
-					fprintf(fp2, "%s\n", buff);
-					fclose(fp2);
-							FILE *fp3 = fopen(filename, "r");
-								printf("\nSTAMPAM LISTU POSLE UPISA\n\n");
-						while (fgets(line, sizeof(line), fp3) != NULL) {
-					printf("%s\n",line);					
-
-				}
-
-					fclose(fp3);
-					printf("Unesen je sajt u bazu\n");
-					strtok_r(buff, " ", &ptr);
-					puts(client_message);
-					puts(buff);
-					puts(ptr);
-					printf("%d\n",strlen(ptr));
-					printf("%d\n",strlen(client_message));					
-					printf("Razbijam liniju od korenskog na ip i naziv\n");
-					if(strcmp(client_message,buff)==0){
-						flag_name=1;
-						printf("Klijent je uneo sajt...trazi IP nazad\n");
-					}
-					
-					if(strcmp(client_message,ptr)==0){
-						flag_ip=1;
-						printf("Klijent je uneo IP...trazi sajt nazad\n");
-					}
-					//puts("evo ti sajt:");
-					 //puts(buff);
-					 //puts("evo ti ip:");
-					 //puts(ptr);
-				printf("FLAG_IP: %d\n", flag_ip);
-		printf("FLAG_NAME: %d\n", flag_name);
-					 if(flag_name==1){
-
-					 puts("Saljem IP\n");
-					 
-					if ((send(client_sock, ptr, strlen(ptr), 0)) < 0) {
 						puts("Send failed\n");
 						return 1;
 					}
-						printf("Poslao sam klojentu IP\n");
-					} else if(flag_ip==1){
-					 puts("Saljem ime\n");
-
-					 if((send(client_sock,buff,strlen(buff),0))<0)
-					 {
-
-					 puts("Send failed\n");
-					 return 1;
-					 }
-						printf("Poslao sam klijentu sajt\n");
-
-					 }else {
-						printf("INVALID INPUT\n");
-						 if((send(client_sock,"Invalid!!!\n",strlen("Invalid!!!\n"),0))<0)
-					 {
-
-					 puts("Send failed\n");
-					 return 1;
-					 }
-		
-						}
-					 
-				} else {
-					printf("Sajt vec postoji\n");
 				}
-
-				
-
-				bzero(buff, sizeof(buff));
-			}else{
-		 if((send(client_sock,"Invalid!!!\n",strlen("Invalid!!!\n"),0))<0)
-					 {
-
-					 puts("Send failed\n");
-					 return 1;
-					 }
-	}
-		}
-		printf("FLAG_IP: %d\n", flag_ip);
-		printf("FLAG_NAME: %d\n", flag_name);
-
-		for (x = 0; x < TOT; x++) {
-			for (y = 0; y < BUF; y++) {
-				ip_s[x][y] = '\0';
-				names[x][y] = '\0';
+			}
+			//clean arrays
+			for (x = 0; x < TOT; x++) {
+				for (y = 0; y < BUF; y++) {
+					ips[x][y] = '\0';
+					websites[x][y] = '\0';
+				}
 			}
 		}
-		printf("Praznim listu\n");
-
-	}
 		if (read_size == 0) {
 			puts("Client disconnected");
 			fflush(stdout);
